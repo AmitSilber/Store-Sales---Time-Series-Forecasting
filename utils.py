8f512ca5257e26f8db0d6b79445e13e0cd1e71d7
danarab4 <danarab4@cs.huji.ac.il>
2023-03-08 10:28:06 +0200

    embedding family into numbers - searching for connection in the "sales"

--- utils.py
+++ utils.py
@@ -5,18 +5,33 @@ import seaborn as sns
 
 train_file = 'train.csv'
 
-df = pd.read_csv(train_file, index_col='id')
-y = df.sales
-x = df.drop('sales', axis=1)
 
-
-def print_histogram(data, feature=None):
-    sns.histplot(data,y = feature)
-    plt.title(f"histogram of {feature}")
+def print_histogram(data):
+    df_melted = data.melt(var_name='column')
+    g = sns.FacetGrid(df_melted, row='column')
+    g.map(plt.hist, 'value')
     plt.show()
 
 
-print(x.family.nunique())
-print(x.groupby('store_nbr').family.size())
+def temporal_date(data):
+    data.date = pd.to_datetime(data.date)
+    return data
 
-#plt.plot(x=x, y=y)
+
+def cluster_family_by_sales_corr(df):
+    plt.figure(figsize=(16, 7))
+    sales = pd.DataFrame({g: s.values for g, s in df.groupby('family').sales})
+    corr = sales.corr()
+    sns.clustermap(corr, cmap='Blues')
+    plt.savefig('family_corr.png')
+
+
+def main():
+    df = pd.read_csv(train_file, index_col=0, parse_dates=True)
+    y = df.sales
+    x = df.drop('sales', axis=1)
+    x = temporal_date(x)
+
+
+if __name__ == '__main__':
+    main()
